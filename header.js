e3d7ac8d9eb59c59780409e44dfa5ff5b9b64e81
sjpcourse <155637438+sjpcourse@users.noreply.github.com>
2024-01-05 22:55:45 +0530

    Webpage

---
+++ header.js
@@ -0,0 +1,25 @@
+document.write('\
+\
+<!-- Navbar -->\
+<nav class="navbar navbar-default navbar-fixed-top">\
+  <div class="container">\
+      <div class="navbar-header">\
+        <a class="navbar-brand" href="#schedule">Slides</a>\
+      </div>\
+\
+  </div>\
+</nav>\
+\
+<!-- Header -->\
+<div id="header" style="text-align:center">\
+  <a href="https://www.iitbhilai.ac.in/">\
+    <img src="/image/Logo_of_IIT_Bhilai.png" class="logo-left">\
+  </a>\
+  <a>\
+    <img src="AI_ML.png" class="logo-right">\
+  </a>\
+  <h1 style="background: rgb(39, 39, 39); font-size: 35px; background: linear-gradient(to right, #f32170, #ff6b08, #cf23cf, #eedd44); -webkit-text-fill-color: transparent; -webkit-background-clip: text;">DS510: AI/ML Lab</h1>\
+  <h3>Indian Institute of Technology (IIT), Bhilai / 2023-24-W</h3>\
+  <div style="clear:both;"></div>\
+</div>\
+');
